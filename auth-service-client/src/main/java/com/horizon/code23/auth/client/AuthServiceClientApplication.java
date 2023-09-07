096d5ea1c6163a18b2048ed5d050bf5f57421d22
Karthikeyan Jayachandran <karthiknjay08@gmail.com>
2023-09-08 01:12:35 +0530

    added gitignore

---
+++ auth-service-client/src/main/java/com/horizon/code23/auth/client/AuthServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.horizon.code23.auth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceClientApplication.class, args);
+	}
+
+}
---
+++ auth-service-client/src/test/java/com/horizon/code23/auth/client/AuthServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizon.code23.auth.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
